41c9995ca270f093bc444bec7148e84b8499f8da
Inquisitor <ssd_@ukr.net>
2016-06-03 13:50:46 +0300

    Fix bugs with reading from file and slightly fix formatting for output

--- prog_2_5/prog_2_5/prog_2_5.cpp
+++ prog_2_5/prog_2_5/prog_2_5.cpp
@@ -21,13 +21,11 @@ int main()
     cout << "Nachalo:" << endl;
     ifstream file("D:\\f1.txt");
     Znak zodiac[n];
-    char str[90];
     char strdd[50], strmm[50], stryy[50];
     //Чтение информации из файла
     for (i = 0; i < n; i++) {
         file.getline(zodiac[i].name, 90);
         file.getline(zodiac[i].zodiac, 90);
-        file.getline(str, 90);
         file.getline(strdd, 50);
         zodiac[i].bday[0] = atoi(strdd);
         file.getline(strmm, 50);
@@ -50,27 +48,24 @@ int main()
     /*конец чтения из бинарного файла*/
     cout << "Result: " << endl;
     for (i = 0; i < n; i++) {
-        cout << resetiosflags(ios::right);
-        cout.setf(ios::left);
-        cout << setw(20) << setfill('.');
-        cout << zodiac2[i].name;
-        cout << zodiac2[i].zodiac;
-        cout.width(20);
-        cout.setf(ios::right);
-        cout << zodiac2[i].bday[0] << '.' << zodiac2[i].bday[1] << '.' << zodiac2[i].bday[2] << '\n';
+        cout << resetiosflags(ios::adjustfield) << setiosflags(ios::left)
+             << setw(20) << setfill('.') << zodiac2[i].name
+             << setw(20) << setfill('.') << zodiac2[i].zodiac
+             << resetiosflags(ios::adjustfield) << setiosflags(ios::right)
+             << setw(2) << setfill('0') << zodiac2[i].bday[0] << '.'
+             << setw(2) << setfill('0') << zodiac2[i].bday[1] << '.'
+             << setw(4) << setfill('0') << zodiac2[i].bday[2] << endl;
     }
     cout.fill('.');
     ofstream out("D:\\f3.txt"); //Запись текстового файла
     for (i = 0; i < n; i++) {
-        out << resetiosflags(ios::right);
-        out.setf(ios::left);
-        out << setw(20) << setfill('.');
-        out << zodiac2[i].name;
-        out << zodiac[i].zodiac;
-        out.width(20);
-        out.setf(ios::right);
-        out << zodiac2[i].bday[0] << '.' << zodiac2[i].bday[1] << '.' << zodiac2[i].bday[2] << '.' << "\n" << endl;
-        out << endl;
+        out << resetiosflags(ios::adjustfield) << setiosflags(ios::left)
+             << setw(20) << setfill('.') << zodiac2[i].name
+             << setw(20) << setfill('.') << zodiac2[i].zodiac
+             << resetiosflags(ios::adjustfield) << setiosflags(ios::right)
+             << setw(2) << setfill('0') << zodiac2[i].bday[0] << '.'
+             << setw(2) << setfill('0') << zodiac2[i].bday[1] << '.'
+             << setw(4) << setfill('0') << zodiac2[i].bday[2] << endl;
     }
     bincin.close();
     cout << endl;
